c8fe2a27077f4c3ae075f4a172a968948d798a4b
romabliski <jakub.romaszewski@gmail.com>
2017-06-13 05:55:49 +0200

    ADD
    ADD

---
+++ Assets/TowerLife.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Timers;
+
+public class TowerLife : MonoBehaviour {
+
+    private static int _mnoznik;
+    public static float zycie = 100000;
+    private float maxzycie = zycie;
+    private float _;
+
+    [SerializeField]
+    private Image Mask;
+    [SerializeField]
+    private Text Licznikczasu;
+
+	void Start () {
+        _mnoznik = PlayerPrefs.GetInt("pkt2");
+        StartCoroutine(tajmer());
+
+	}
+	void Update () {
+        HBar();
+	}
+    private void HBar()
+    {
+        Mask.fillAmount = skalowanie(maxzycie, zycie);
+    }
+    private float skalowanie(float max, float min)
+    {
+        return min / max;
+    }
+    public IEnumerator tajmer()
+    {
+
+        yield return new WaitForSeconds(1f);
+    }
+}
