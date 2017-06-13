655f3912335d489af1444b7e1dc64cd82a4a7547
romabliski <jakub.romaszewski@gmail.com>
2017-06-13 05:56:20 +0200

    2
    2

---
+++ Assets/Main Menu/Mainmenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mainmenu : MonoBehaviour {
+
+public GameObject quiz;
+public void Start()
+{
+    Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+}
+	public void QuitGame() {
+		Application.Quit ();
+	}
+
+	public void StarGame() {
+		Application.LoadLevel ("2");
+	}
+
+}
